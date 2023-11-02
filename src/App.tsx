79acb7b32a7f7b02904c7590b74c02d70a6714e5
Minminjamin <0429el@gmail.com>
2023-11-02 18:23:21 +0900

    feature : react-router-dom 도입과 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import ApplicationForm from "./pages/ApplicationForm/ApplicationForm";
 import Bus from "./pages/Bus/Bus";
@@ -6,16 +6,22 @@ import Commute from "./pages/Commute/Commute";
 import Dormitory from "./pages/Dormitory/Dormitory";
 import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> */}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/bus" element={<Bus />} />
+      </Routes>
+
       {/* <Login /> */}
       {/* <Commute /> */}
       {/* <Dormitory /> */}
       {/* <Bus /> */}
-      <ApplicationForm />
+      {/* <ApplicationForm /> */}
     </div>
   );
 }
