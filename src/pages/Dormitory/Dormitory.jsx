b55a20a898385662cea8a8e030aa254404c4351b
Minminjamin <0429el@gmail.com>
2023-11-02 22:11:53 +0900

    feature : Dormitory 컴포넌트 기본 레이아웃 + 신청 여부 클릭에 따른 분기 처리

--- src/pages/Dormitory/Dormitory.jsx
+++ src/pages/Dormitory/Dormitory.jsx
@@ -2,19 +2,31 @@ import React, { useState } from "react";
 import Warning from "../../components/Warning/Warning";
 import "./Dormitory.scss";
 import Bus from "../Bus/Bus";
+import ApplicationForm from "../ApplicationForm/ApplicationForm";
 
 const Dormitory = () => {
   const [isOpen, setIsOpen] = useState(true);
+  const [isBus, setIsBus] = useState(false);
+
+  const onHandleApplied = () => {
+    setIsBus(true);
+    setIsOpen(false);
+  };
 
   return (
-    <div className="wrap">
+    <div className="dormitoryWrap">
       {isOpen ? (
         <Warning setIsOpen={setIsOpen}>
           <span>사감 선생님 및 담임선생님께</span>
           <span>외출 또는 외박 허가를 받겠습니까?</span>
+          <span onClick={onHandleApplied} className="alreadyApplied">
+            이미 외출 외박 신청을 받으셨다면 여기를 클릭해주세요.
+          </span>
         </Warning>
-      ) : (
+      ) : isBus ? (
         <Bus />
+      ) : (
+        <ApplicationForm />
       )}
     </div>
   );
