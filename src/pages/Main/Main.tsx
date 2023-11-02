e6bcaab6f486e14e79fe7f5949a9bb47cc53ed80
Minminjamin <0429el@gmail.com>
2023-11-02 16:35:39 +0900

    feature : 로그인 페이지로 리다이렉트

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Main.scss";
+import { useNavigate } from "react-router-dom";
 
 const path: string = process.env.PUBLIC_URL;
 
 const Main = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/login");
+    }, 3000);
+  }, []);
+
   return (
     <main>
       <img src={`${path}/image/main/logo.png`} alt="counBus 로고 이미지" />
