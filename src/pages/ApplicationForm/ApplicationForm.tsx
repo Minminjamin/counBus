b384d5f5ea826b9b0f1c887b1a561f391d3a4509
Minminjamin <0429el@gmail.com>
2023-11-03 09:02:58 +0900

    feature : 신청서를 메일로 전송

--- src/pages/ApplicationForm/ApplicationForm.tsx
+++ src/pages/ApplicationForm/ApplicationForm.tsx
@@ -1,54 +1,84 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./ApplicationForm.scss";
+import emailjs from "@emailjs/browser";
 
 const ApplicationForm = () => {
+  const form = useRef<HTMLFormElement>(null);
+
+  const onHandleEmailSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          process.env.REACT_APP_EMAILJS_SERVICE_ID as string,
+          process.env.REACT_APP_EMAILJS_TEMPLATE_ID as string,
+          form.current,
+          process.env.REACT_APP_EMAILJS_PUBLIC_KEY as string
+        )
+        .then(
+          function (response) {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          function (error) {
+            console.log("FAILED...", error);
+          }
+        );
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={onHandleEmailSubmit} ref={form}>
       <h1>외출 · 외박 신청서 </h1>
 
       <div className="inputForm">
         <label>이름</label>
-        <input type="text" placeholder="내 답변" />
+        <input type="text" placeholder="내 답변" name="from_name" />
+      </div>
+
+      <div className="inputForm">
+        <label>외출, 외박</label>
+        <input type="text" placeholder="내 답변" name="type" />
       </div>
 
       <div className="inputForm">
         <label>출사 일시</label>
-        <input type="date" placeholder="내 답변" />
+        <input type="date" placeholder="내 답변" name="out_time" />
       </div>
 
       <div className="inputForm">
         <label>귀사 일시</label>
-        <input type="date" placeholder="내 답변" />
+        <input type="date" placeholder="내 답변" name="come_time" />
       </div>
 
       <div className="inputForm">
         <label>상세 사유</label>
-        <input type="text" placeholder="내 답변" />
+        <input type="text" placeholder="내 답변" name="reason" />
       </div>
 
       <div className="inputForm">
         <label>장소</label>
-        <input type="text" placeholder="내 답변" />
+        <input type="text" placeholder="내 답변" name="place" />
       </div>
 
       <div className="inputForm">
         <label>증빙 서류</label>
-        <input type="text" placeholder="내 답변" />
+        <input type="text" placeholder="내 답변" name="document" />
       </div>
 
       <div className="inputForm">
         <label>자택 주소 및 전화 번호</label>
-        <input type="text" placeholder="내 답변" />
+        <input type="text" placeholder="내 답변" name="home_address" />
       </div>
 
       <div className="inputForm">
         <label>보호자 HP</label>
-        <input type="text" placeholder="내 답변" />
+        <input type="text" placeholder="내 답변" name="parents_phone" />
       </div>
 
       <div className="btn">
         <button>수정</button>
-        <button>제출</button>
+        <button type="submit">제출</button>
       </div>
     </form>
   );
