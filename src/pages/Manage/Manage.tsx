59a5122dbec1b19ef923e72cd4b161abde39a19c
Minminjamin <0429el@gmail.com>
2023-11-07 22:10:48 +0900

    style : Manage 컴포넌트 기본 퍼플리싱

--- src/pages/Manage/Manage.tsx
+++ src/pages/Manage/Manage.tsx
@@ -3,6 +3,7 @@ import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { firestore } from "../../libs/Firebase";
+import "./Manage.scss";
 
 const Manage = () => {
   const navigate = useNavigate();
@@ -52,18 +53,19 @@ const Manage = () => {
   }, []);
 
   return (
-    <div>
+    <div className="manageWrap">
       <h1>학생 관리 페이지</h1>
-
+      {/* 
       <table>
-        <tr>
+        <tr className="topMenu">
           <th scope="col">학번</th>
           <th scope="col">날짜</th>
           <th scope="col">사유</th>
+          <th scope="col"></th>
         </tr>
         {dbData &&
           dbData.map((item: any, idx: string) => (
-            <tr key={idx}>
+            <tr key={idx} className="info">
               <td>{item.class}</td>
               <td>{item.out_time}</td>
               <td>{item.reason}</td>
@@ -72,7 +74,32 @@ const Manage = () => {
               </td>
             </tr>
           ))}
-      </table>
+      </table> */}
+
+      <section>
+        <div className="top">
+          <span>학번</span>
+          <span>날짜</span>
+          <span>사유</span>
+        </div>
+
+        <div className="studentInfoWrap">
+          {dbData &&
+            dbData.map((item: any, idx: string) => (
+              <section key={idx} className="info">
+                <span>{item.class}</span>
+                <span>{item.out_time}</span>
+                <span>{item.reason.slice(0, 8)}</span>
+
+                <button>삭제</button>
+              </section>
+            ))}
+        </div>
+
+        <div className="btnSet">
+          <button>다운로드</button>
+        </div>
+      </section>
     </div>
   );
 };
