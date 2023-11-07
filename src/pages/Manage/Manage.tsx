4e875ba2db8d867391974750a5c0412125778929
Minminjamin <0429el@gmail.com>
2023-11-07 19:21:02 +0900

    feature : Manage 컴포넌트에 외출, 외박 데이터 fetch

--- src/pages/Manage/Manage.tsx
+++ src/pages/Manage/Manage.tsx
@@ -1,9 +1,13 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import React, { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { firestore } from "../../libs/Firebase";
 
 const Manage = () => {
   const navigate = useNavigate();
+
+  const [dbData, setDbData] = useState<any>([]);
 
   useEffect(() => {
     const auth = getAuth();
@@ -29,9 +33,46 @@ const Manage = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(
+        collection(firestore, "student_data")
+      );
+
+      const data: any = [];
+
+      querySnapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+
+      setDbData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>manage page</h1>
+      <h1>학생 관리 페이지</h1>
+
+      <table>
+        <tr>
+          <th scope="col">학번</th>
+          <th scope="col">날짜</th>
+          <th scope="col">사유</th>
+        </tr>
+        {dbData &&
+          dbData.map((item: any, idx: string) => (
+            <tr key={idx}>
+              <td>{item.class}</td>
+              <td>{item.out_time}</td>
+              <td>{item.reason}</td>
+              <td>
+                <button>삭제</button>
+              </td>
+            </tr>
+          ))}
+      </table>
     </div>
   );
 };
