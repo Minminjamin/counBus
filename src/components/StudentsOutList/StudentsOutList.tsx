5ad1901e8b219cd94989937f528564e6e01172f7
Minminjamin <0429el@gmail.com>
2023-11-07 22:15:59 +0900

    refact & move : 지금까지 작업한 amage 컴포넌트를 studentsOutListWrap 컴포넌트로 분리

---
+++ src/components/StudentsOutList/StudentsOutList.tsx
@@ -0,0 +1,58 @@
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { firestore } from "../../libs/Firebase";
+import "./StudentsOutList.scss";
+
+const StudentsOutList = () => {
+  const [dbData, setDbData] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(
+        collection(firestore, "student_data")
+      );
+
+      const data: any = [];
+
+      querySnapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+
+      setDbData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="studentsOutListWrap">
+      <h1>학생 관리 페이지</h1>
+      <section>
+        <div className="top">
+          <span>학번</span>
+          <span>날짜</span>
+          <span>사유</span>
+        </div>
+
+        <div className="studentInfoWrap">
+          {dbData &&
+            dbData.map((item: any, idx: string) => (
+              <section key={idx} className="info">
+                <span>{item.class}</span>
+                <span>{item.out_time}</span>
+                <span>{item.reason.slice(0, 8)}</span>
+
+                <button>삭제</button>
+              </section>
+            ))}
+        </div>
+
+        <div className="btnSet">
+          <button>다운로드</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default StudentsOutList;
--- src/pages/Manage/Manage.tsx
+++ src/pages/Manage/Manage.tsx
@@ -2,6 +2,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import StudentsOutList from "../../components/StudentsOutList/StudentsOutList";
 import { firestore } from "../../libs/Firebase";
 import "./Manage.scss";
 
@@ -34,72 +35,9 @@ const Manage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(
-        collection(firestore, "student_data")
-      );
-
-      const data: any = [];
-
-      querySnapshot.forEach((doc) => {
-        data.push(doc.data());
-      });
-
-      setDbData(data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="manageWrap">
-      <h1>학생 관리 페이지</h1>
-      {/* 
-      <table>
-        <tr className="topMenu">
-          <th scope="col">학번</th>
-          <th scope="col">날짜</th>
-          <th scope="col">사유</th>
-          <th scope="col"></th>
-        </tr>
-        {dbData &&
-          dbData.map((item: any, idx: string) => (
-            <tr key={idx} className="info">
-              <td>{item.class}</td>
-              <td>{item.out_time}</td>
-              <td>{item.reason}</td>
-              <td>
-                <button>삭제</button>
-              </td>
-            </tr>
-          ))}
-      </table> */}
-
-      <section>
-        <div className="top">
-          <span>학번</span>
-          <span>날짜</span>
-          <span>사유</span>
-        </div>
-
-        <div className="studentInfoWrap">
-          {dbData &&
-            dbData.map((item: any, idx: string) => (
-              <section key={idx} className="info">
-                <span>{item.class}</span>
-                <span>{item.out_time}</span>
-                <span>{item.reason.slice(0, 8)}</span>
-
-                <button>삭제</button>
-              </section>
-            ))}
-        </div>
-
-        <div className="btnSet">
-          <button>다운로드</button>
-        </div>
-      </section>
+    <div className="managewrap">
+      <StudentsOutList />
     </div>
   );
 };
