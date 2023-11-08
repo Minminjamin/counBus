fbbf41eced847456b82ba3653807f6f7c6fd947f
Minminjamin <0429el@gmail.com>
2023-11-08 14:44:57 +0900

    feature : excel로 해당 월의 학생 외출, 외박 데이터 다운 기능 추가

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -1,6 +1,10 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { BsDownload } from "react-icons/bs";
+import { firestore } from "../../libs/Firebase";
 import "./Download.scss";
+import ExcelJS from "exceljs";
+import { saveAs } from "file-saver";
 
 const Download = () => {
   const [yearDate, setYearDate] = useState([]);
@@ -26,16 +30,68 @@ const Download = () => {
     setYearDate(months);
   }, []);
 
+  const onHanldeClickDownload = async (selectDate: string) => {
+    const date = selectDate.split("년 ").join("-").split("월").join("");
+
+    const querySnapshot = await getDocs(
+      query(
+        collection(firestore, "student_data"),
+        where("out_time", ">=", date),
+        where("out_time", "<=", date + "31")
+      )
+    );
+
+    const data: any = [];
+
+    querySnapshot.forEach((doc) => {
+      data.push(doc.data());
+    });
+
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet(
+      `${selectDate} 학생 외출 · 외박 기록`
+    );
+
+    worksheet.addRow([
+      "학번",
+      "이름",
+      "외출/외박",
+      "출사 시간",
+      "귀사 시간",
+      "장소",
+      "사유",
+      "증빙 서류",
+      "보호자 전화번호",
+    ]);
+
+    data.forEach((item: any) => {
+      worksheet.addRow([
+        item.class,
+        item.from_name,
+        item.type,
+        item.out_time,
+        item.come_time,
+        item.place,
+        item.reason,
+        item.document,
+        item.parents_phone,
+      ]);
+    });
+
+    const buffer = await workbook.xlsx.writeBuffer();
+    saveAs(new Blob([buffer]), `${selectDate} 학생 외출 · 외박 기록.xlsx`);
+  };
+
   return (
     <section className="downloadWrap">
       {yearDate &&
-        yearDate.map((item, idx) => (
+        yearDate.map((item: string, idx: number) => (
           <article key={idx}>
             <div>
               <span>Excel</span>
               <span>{item} 외출 · 외박 기록</span>
             </div>
-            <BsDownload />
+            <BsDownload onClick={() => onHanldeClickDownload(item)} />
           </article>
         ))}
       {/* <h1>down</h1> */}
