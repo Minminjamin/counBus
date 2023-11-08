dd94d4af30e659cf14a51428c9cd31835c9d27aa
Minminjamin <0429el@gmail.com>
2023-11-08 18:39:58 +0900

    feature : LogoutLayout 컴포넌트 생성 & 로그아웃 기능

---
+++ src/components/LogoutLayout/LogoutLayout.tsx
@@ -0,0 +1,25 @@
+import { getAuth, signOut } from "firebase/auth";
+import React, { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
+import "./LogoutLayout.scss";
+
+const LogoutLayout = ({ children }: { children: ReactNode }) => {
+  const navigate = useNavigate();
+
+  const onHandleClickLogout = () => {
+    const auth = getAuth();
+
+    signOut(auth).then(() => {
+      navigate("/login");
+    });
+  };
+
+  return (
+    <div className="logoutLayoutWrap">
+      {children}
+      <button onClick={onHandleClickLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default LogoutLayout;
