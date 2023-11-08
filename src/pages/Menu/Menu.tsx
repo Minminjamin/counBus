f89c7455f9d544691d3c13970767981b01b26b8a
Minminjamin <0429el@gmail.com>
2023-11-08 21:36:18 +0900

    feature : Menu의 버튼 클릭 시 이동

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import LogoutLayout from "../../components/LogoutLayout/LogoutLayout";
 import "./Menu.scss";
 
 const Menu = () => {
+  const navigate = useNavigate();
+
   return (
     <LogoutLayout>
       <div className="btnSet">
-        <button>
+        <button onClick={() => navigate("/application")}>
           외출 · 외박
-          <br /> 신청서 작성
+          <br />
+          신청서 작성
         </button>
-        <button>버스 시간표</button>
+        <button onClick={() => navigate("/bus")}>버스 시간표</button>
       </div>
     </LogoutLayout>
   );
