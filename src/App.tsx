e3f0de59d1f5f4402d17847925f7a769d7953345
Minminjamin <0429el@gmail.com>
2023-11-08 21:34:34 +0900

    refact & move : ui 변경에 따른 라우팅 경로 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.scss";
-import Commute from "./pages/Commute/Commute";
-import Dormitory from "./pages/Dormitory/Dormitory";
 import Login from "./pages/Login/Login";
 import Main from "./pages/Main/Main";
 import { Route, Routes } from "react-router-dom";
 import Permission from "./pages/Permission/Permission";
 import Manage from "./pages/Manage/Manage";
 import Menu from "./pages/Menu/Menu";
+import Bus from "./pages/Bus/Bus";
+import ApplicationForm from "./pages/ApplicationForm/ApplicationForm";
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dormitory" element={<Dormitory />} />
-        <Route path="/commute" element={<Commute />} />
+        <Route path="/bus" element={<Bus />} />
+        <Route path="/application" element={<ApplicationForm />} />
         <Route path="/permission" element={<Permission />} />
         <Route path="/manage" element={<Manage />} />
       </Routes>
